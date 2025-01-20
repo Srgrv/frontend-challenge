ea8e85c5393925033e169a4c545552fbcc4c9cd2
Sergey Georgiev <sgrgv_art@mail.ru>
2025-01-20 18:22:20 +0300

    Улучшен компонент header

--- cat-pinterest/src/components/Header.js
+++ cat-pinterest/src/components/Header.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+const menuItems = [
+  { text: "Все котики", width: "120px" },
+  { text: "Любимые котики", width: "173px" },
+];
+
 export const Header = () => {
   return (
     <header className="h-16 bg-[#2196F3] flex text-white font-roboto font-normal text-[14px] leading-[21px] tracking-[0.25px]">
-      <div className="hover:bg-[#1E88E5] w-[120px] flex items-center justify-center">
-        Все котики
-      </div>
-      <div className="hover:bg-[#1E88E5] w-[173px] flex items-center justify-center">
-        Любимые котики
-      </div>
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          className="hover:bg-[#1E88E5] flex items-center justify-center"
+          style={{ width: item.width }}
+        >
+          {item.text}
+        </div>
+      ))}
     </header>
   );
 };
